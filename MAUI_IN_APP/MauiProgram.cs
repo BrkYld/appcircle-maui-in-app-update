5eda44303ada86b0f1f3f098acc94cb6b526a7c0
burak.yildirim <burak_yildirim_official@hotmail.com>
2025-01-05 19:40:31 +0300

    change env

--- MAUI_IN_APP/MauiProgram.cs
+++ MAUI_IN_APP/MauiProgram.cs
@@ -19,9 +19,9 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
-		Environment.SetEnvironmentVariable("IOS_STORE_SECRET", "MjA5ODBjZTktNmVhOS00ODExLWE4ZDctMmE4YTZhMGJlYjY4fDZmYmQwOTRjLTE4Y2ItNGJjZC1iYzZmLWNmMmFlZTc1Mjk4Mg==");
+		Environment.SetEnvironmentVariable("IOS_STORE_SECRET", "NGIyZjdiNzUtNmZmYi00YTgxLWFiM2QtYjkyNDg4MjhjYzAwfDZmYmQwOTRjLTE4Y2ItNGJjZC1iYzZmLWNmMmFlZTc1Mjk4Mg==");
 		Environment.SetEnvironmentVariable("ANDROID_STORE_SECRET", "NWViMWFkZjktMGE4My00OGMxLTk2NDMtZjVlMjMzZmMyNzVlfDZmYmQwOTRjLTE4Y2ItNGJjZC1iYzZmLWNmMmFlZTc1Mjk4Mg==");
-		Environment.SetEnvironmentVariable("STORE_URL", "https://sarp.store.appcircle.io");
+		Environment.SetEnvironmentVariable("STORE_URL", "https://store.store.appcircle.io");
 		Environment.SetEnvironmentVariable("IOS_PROFILE_ID", "f2487ca0-188c-490f-9c28-ab0c41d15a3e");
 		Environment.SetEnvironmentVariable("ANDROID_PROFILE_ID", "8f5acd24-dd25-4464-b49c-655630db9f2b");
 		return builder.Build();
