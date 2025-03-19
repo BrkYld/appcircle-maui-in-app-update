2b04ca452f5aef984644b4bd0502fe67e5aef9d9
burak.yildirim <burak_yildirim_official@hotmail.com>
2025-03-19 21:27:45 +0300

    update helper v4

--- MAUI_IN_APP/MauiProgram.cs
+++ MAUI_IN_APP/MauiProgram.cs
@@ -15,7 +15,6 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
-		builder.Services.AddSingleton<IApkInstaller, ApkInstaller>();
 
 #if DEBUG
 		builder.Logging.AddDebug();
